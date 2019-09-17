7a576a39432ca1bf45826eecdf099289fe69cf9b
John Clark <bennyclarkup@gmail.com>
2019-09-17 15:35:29 -0500

    bkakd

---
+++ TrueCoders/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TrueCoders
+{
+    public class People
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HairColor { get; set; }
+
+        public People(string fN, string lN, int age, string hC)
+        {
+            Age = age;
+            FirstName = fN;
+            LastName = lN;
+            HairColor = hC;
+        }
+    }
+}
---
+++ TrueCoders/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            List<People> trueCoders = new List<People>();
+
+            People BB = new People("BillyBob", "Bohannon", 21 , "Scotch");
+            People JJ = new People("JimJoe", "Collins", 16 , "Burgundy");
+            People IS = new People("Rocky", "Balboa", 24 , "Black");
+
+            trueCoders.Add(BB);
+            trueCoders.Add(JJ);
+            trueCoders.Add(IS);
+
+            foreach(var coder in trueCoders)
+            {
+                Console.WriteLine("Name: " + coder.FirstName + " "  + coder.LastName + " " + "Age: " +  coder.Age + " " + "Hair Color: " + coder.HairColor);
+            }
+
+        }
+    }
+}
